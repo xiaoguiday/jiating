56e10e58e0a433312206ebecf3888f0758c242f2
xiaoguiday <133844933+xiaoguiday@users.noreply.github.com>
2025-08-21 19:32:07 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -23,7 +23,7 @@ https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/sub
 `
 
 let urls = [];
-let subConverter = "sub.cmliussss.com"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subConverter = "suburl.v1.mk"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subConfig = "https://raw.githubusercontent.com/xiaoguiday/quectel-rgmii-toolkit/refs/heads/main/lianshidaili.ini"; //订阅配置文件
 let subProtocol = 'https';
 
@@ -828,4 +828,5 @@ async function KV(request, env, txt = 'ADD.txt', guest) {
 		});
 	}
 }
+
 
